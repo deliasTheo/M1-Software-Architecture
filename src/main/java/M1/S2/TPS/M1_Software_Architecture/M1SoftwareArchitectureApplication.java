8b30857a31c0e6d95851be3e259025f9f662a86e
deliasTheo <81029828+deliasTheo@users.noreply.github.com>
2026-01-26 10:35:27 +0100

    first commit

---
+++ src/main/java/M1/S2/TPS/M1_Software_Architecture/M1SoftwareArchitectureApplication.java
@@ -0,0 +1,13 @@
+package M1.S2.TPS.M1_Software_Architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1SoftwareArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1SoftwareArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/M1/S2/TPS/M1_Software_Architecture/M1SoftwareArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package M1.S2.TPS.M1_Software_Architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1SoftwareArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
